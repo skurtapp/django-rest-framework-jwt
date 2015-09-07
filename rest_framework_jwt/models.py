5aa0df37aa01b337f7f0f3155119838f9edc6aa3
Mirai Akagawa <mirai418@me.com>
2015-09-07 11:02:59 -0700

    add ParseUserProxies

--- rest_framework_jwt/models.py
+++ rest_framework_jwt/models.py
@@ -1,1 +1,11 @@
-# Just to keep things like ./manage.py test happy
+from django.db import models
+
+
+class ParseUserProxy(models.Model):
+    """
+    A proxy model to the Parse User table
+    """
+    objectId = models.CharField(unique=True, max_length=16, primary_key=True)
+
+    def __str__(self):
+        return self.objectId
